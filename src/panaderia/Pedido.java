82f8cc13488fd5e61bd3e156f3229e2b6e4fceb4
Default <you@example.com>
2018-05-23 18:20:20 -0300

    Se modifico la clase pedido

--- src/panaderia/Pedido.java
+++ src/panaderia/Pedido.java
@@ -1,5 +1,8 @@
 package panaderia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +14,37 @@ package panaderia;
  * @author utku30
  */
 public class Pedido {
+    
+    private List<DetallePedido> detalles;
+
+    public Pedido() {
+        detalles = new ArrayList();
+    }
+    
+    public Pedido(List<DetallePedido> detalles) {
+        this.detalles = detalles; 
+    }
+    
+    public void cancelarDetalle(DetallePedido detalle) {
+        detalles.remove(detalle);
+    }
+    
+    public float calcularTotalPedido() {
+        float totalPedido = 0.0f;
+        for(DetallePedido detalle : detalles) {
+            totalPedido += detalle.calcularTotalDetalle();
+        }
+        
+        return totalPedido;
+    }
+
+    @Override
+    public String toString() {
+        String retornoFuncion = "";
+        for(DetallePedido detalle : detalles) {
+            retornoFuncion += detalle.toString() + "\n";
+        }
+        return retornoFuncion;
+    }
     
 }
